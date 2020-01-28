01c12850b69c94077ce2014bd589ef016fd1f939
etienne pierrot <etienne.pierrot@gmail.com>
2020-01-28 09:34:03 +0100

    add ref

---
+++ src/references.fsx
@@ -0,0 +1,4 @@
+#r "../../../.nuget/packages/fable.react/5.1.0/lib/netstandard2.0/Fable.React.dll"
+#r "../../../.nuget/packages/fable.core/3.0.0/lib/netstandard2.0/Fable.Core.dll"
+#r "../../../.nuget/packages/fable.elmish/3.0.0/lib/netstandard2.0/Fable.Elmish.dll"
+#r "../../../.nuget/packages/fable.elmish.react/3.0.1/lib/netstandard2.0/Fable.Elmish.React.dll"
