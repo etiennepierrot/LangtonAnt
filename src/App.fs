a80694a3a8090c5933780a25d76d8e601c3e8c41
etienne pierrot <etienne.pierrot@gmail.com>
2020-02-02 20:43:37 +0100

    add style

--- src/App.fs
+++ src/App.fs
@@ -7,8 +7,6 @@ open Fable.React.Props
 open Elmish
 open Elmish.React
 open Fulma
-open Fulma.Elmish
-open Shortcuts
 
 
 let board = GenerateBoard
@@ -29,10 +27,11 @@ let initialState =
       IsRunning = false }
 
 let NewState(state: State) = { state with Game = Play state.Game }
-let RunSimulation (state : State) (iteration : int) =
+
+let RunSimulation (state: State) (iteration: int) =
     let game = PlayRec state.Game iteration
     { state with Game = game }
-    
+
 type Message =
     | Move of State
     | Run
@@ -48,58 +47,81 @@ let update (msg: Message) (model: State) =
     | Stop -> { model with IsRunning = false }, Cmd.none
     | SetIterationNumber i -> { model with Game = { model.Game with IterationNumber = i } }, Cmd.none
     | Run -> { model with IsRunning = true }, Cmd.none
-    | RunWithoutAnimation -> (RunSimulation model model.Game.IterationNumber ), Cmd.none
+    | RunWithoutAnimation -> (RunSimulation model model.Game.IterationNumber), Cmd.none
 
 
 let view (model: State) (dispatch: Message -> unit) =
-    let tableRow xs =
-        tr [] [ for x in xs -> td [] [ x ] ]
-
-    Container.container [Container.IsWideScreen]
-        [ div [ ClassName "block" ]
-              [ label [] [ str "Iterations" ]
-                input
-                    [ Class "input"
-                      Value model.Game.IterationNumber
-                      OnChange(fun ev ->
-                          ev.target?value
-                          |> int
-                          |> SetIterationNumber
-                          |> dispatch) ]
-
-                Button.button [Button.Color IsSuccess; Button.Props[ OnClick(fun _ -> dispatch Run) ]] [str "Run"]
-                Button.button [Button.Color IsSuccess; Button.Props[ OnClick(fun _ -> dispatch RunWithoutAnimation) ]] [str "Run without animation"]
-                Button.button [Button.Color IsDanger; Button.Props[ OnClick(fun _ -> dispatch Stop) ]] [str "Stop"]
-                
-                table []
-                    [ for row in model.Game.Board.ConvertBoardToArray2D ->
-                        tableRow
-                            [ for n in row ->
-                                div
-                                    [ Style
-                                        [ BackgroundColor n
-                                          Width "10px"
-                                          Height "10px" ] ] [ str ("") ] ] ] ]
-          div
-              [ Id "Chart1"
-                Ref(fun element ->
-                    // Ref is trigger with null once for stateless element so we need to wait for the second trigger
-                    if not (isNull element) then
-                        // The div has been mounted check if this is the first time
-                        if (model.IsRunning && model.Game.IterationNumber > 0) then
-                            model
-                            |> NewState
-                            |> Move
-                            |> dispatch) ] [] ]
-
-
-
-// App
+    
+    let game = model.Game
+    let array2D = game.Board.ConvertBoardToArray2D
+    let sizeArray =  (array2D.Length * 10).ToString() + "px"
+    let tableRow xs = tr [] [ for x in xs -> td [ Class "is-paddingless" ][x] ]
+    
+    div []
+        [
+          Content.content []
+              [
+                div [
+                        Ref(fun element ->
+                        // Ref is trigger with null once for stateless element so we need to wait for the second trigger
+                        if not (isNull element) then
+                            // The div has been mounted check if this is the first time
+                            if (model.IsRunning && model.Game.IterationNumber > 0) then
+                                model
+                                |> NewState
+                                |> Move
+                                |> dispatch
+                             else if( model.IsRunning && model.Game.IterationNumber = 0)
+                                then dispatch Stop
+                         )                               
+                        ]
+                    [
+                      Field.div []
+                          [ Label.label [] [ str "Iterations :" ]
+                            Control.div [] [ Input.text [
+                                                          Input.Placeholder "Ex: 100"
+                                                          Input.Value (game.IterationNumber |> string )
+                                                          Input.OnChange (fun ev -> ev.target?value
+                                                                                    |> int
+                                                                                    |> SetIterationNumber
+                                                                                    |> dispatch)
+                                                          ] ] ]
+                     
+                      Button.button
+                          [
+                            Button.OnClick (fun _ -> dispatch Run)
+                          ] [str "Run"]
+                      Button.button
+                          [ Button.Color IsSuccess
+                            Button.OnClick (fun _ -> dispatch RunWithoutAnimation)  
+                          ] [str "Run without animation"]
+                      Button.button
+                          [ Button.Color IsDanger
+                            Button.OnClick (fun _ -> dispatch Stop)  
+                          ] [str "Stop"]
+                      
+                    ]
+                    
+                table[
+                    Class "table"    
+                    Style [
+                            Width sizeArray
+                            Height sizeArray]]
+                    [tbody [] [ for row in game.Board.ConvertBoardToArray2D ->
+                                tableRow [ for color in row ->
+                                            div [Style
+                                                    [
+                                                      BackgroundColor color
+                                                      Width "10px"
+                                                      Height "10px"
+                                                      ]] [str ""] ] ]
+                      ]
+              ]
+                    
+                     
+        ]
+        
 Program.mkProgram init update view
 |> Program.withReactBatched "elmish-app"
 |> Program.withConsoleTrace
 |> Program.run
-
-
-//let state = initialState |> NewState |> NewState |> NewState
-//ConvertBoardToArray2D state.Game.Board
